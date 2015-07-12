716a886ab4ac9fe0c07438829ad8ad9cbe9c2d57
nagahama0427 <zodiacal.taurus@gmail.com>
2015-07-12 22:27:45 +0900

    test2

---
+++ java/sample/src/jp/co/technica/imple/make_method/Main.java
@@ -0,0 +1,14 @@
+package jp.co.technica;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+        System.out.println("Hello World2 !!");
+        System.out.println("Hello World3 !!");
+        System.out.println("Hello World4 !!");
+    }
+}
