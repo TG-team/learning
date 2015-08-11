02b21418959826146651be2c7d9018027589928b
taisuke-uchida <taisuke_uchida@technical-g.co.jp>
2015-08-11 20:38:40 +0900

    「クラスを作ろう」->「継承を防ぐ」->Mainクラスの修正

--- java/sample/src/jp/co/technica/imple/make_clazz/extinction/Main.java
+++ java/sample/src/jp/co/technica/imple/make_clazz/extinction/Main.java
@@ -11,48 +11,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        normalPlay();
-
-        System.out.println();
-        System.out.println("チートキャラを作りました");
-
-        cheatPlay();
+        play(new Player(), createEnemys());
+        System.out.println("●チートキャラを作りました");
+        play(new Cheater(), createEnemys());
 
     }
 
-    private static void normalPlay() {
-        GameCharacter player = new Player();
-        GameCharacter[] enemys = { new Enemy1(), new Enemy2(), new Boss() };
-
-        System.out.println("========ゲーム開始========");
-        for (GameCharacter enemy : enemys) {
-            String enemyName = enemy.getClass().getSimpleName();
-            System.out.println(enemyName + "が現れた");
-
-            while (isAlive(player)) {
-                System.out.println("--プレイヤーの攻撃");
-                player.attack(enemy);
-
-                if (isAlive(enemy)) {
-                    System.out.println("----" + enemyName + "の攻撃");
-                    enemy.attack(player);
-                } else {
-                    System.out.println(enemyName + "を倒した");
-                    break;
-                }
-            }
-
-            if (!isAlive(player)) {
-                System.out.println("========ゲームオーバー========");
-                return;
-            }
-        }
-        System.out.println("========ゲームクリア========");
+    private static GameCharacter[] createEnemys() {
+        return new GameCharacter[] { new Enemy1(), new Enemy2(), new Boss() };
     }
 
-    private static void cheatPlay() {
-        GameCharacter player = new Cheater();
-        GameCharacter[] enemys = { new Enemy1(), new Enemy2(), new Boss() };
+    private static void play(GameCharacter player, GameCharacter[] enemys) {
 
         System.out.println("========ゲーム開始========");
         for (GameCharacter enemy : enemys) {
