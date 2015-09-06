6343847615d4a804e47cd5f0592f6e822f66cf8a
taisuke-uchida <taisuke_uchida@technical-g.co.jp>
2015-09-06 16:06:20 +0900

    「さぁメソッドを作ろう」->「繰り返し処理」、「パフォーマンスを考慮しよう」

---
+++ java/sample/src/jp/co/technica/imple/make_method/branch/Main.java
@@ -0,0 +1,98 @@
+package jp.co.technica.imple.make_method.branch;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        instanceSpeed();
+
+        stringConcatenation();
+
+        autoboxing();
+
+    }
+
+    private static void instanceSpeed() {
+        System.out.println("===========instanceSpeed===========");
+
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("StringBuilder#append : ").append(i);
+        }
+        System.out.println("Time(In Loop) : "
+                + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1000000; i++) {
+            sb.append("StringBuilder#append : ").append(i);
+            sb.setLength(0);
+        }
+        System.out.println("Time(Out Loop) : "
+                + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            try {
+                FileInputStream fis = new FileInputStream("c:\\sample.sample");
+            } catch (FileNotFoundException e) {
+            }
+        }
+        System.out.println("Time(In Try-catch) : "
+                + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        try {
+            for (int i = 0; i < 10000; i++) {
+                FileInputStream fis = new FileInputStream("c:\\sample.sample");
+            }
+        } catch (FileNotFoundException e) {
+        }
+        System.out.println("Time(Out Try-catch) : "
+                + (System.currentTimeMillis() - time));
+    }
+
+    private static void stringConcatenation() {
+        System.out.println("===========stringConcatenation===========");
+
+        long time = System.currentTimeMillis();
+        String result = "";
+        for (int i = 0; i < 10000; i++) {
+            result += Integer.toString(i);
+        }
+        System.out.println("Time(+=) : " + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 10000; i++) {
+            builder.append(Integer.toString(i));
+        }
+        System.out.println("Time(StringBuilder#append) : "
+                + (System.currentTimeMillis() - time));
+
+    }
+
+    private static void autoboxing() {
+        System.out.println("===========autoboxing===========");
+
+        long time = System.currentTimeMillis();
+        long sum1 = 0;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            sum1 += i;
+        }
+        System.out.println("Time(primitive) : "
+                + (System.currentTimeMillis() - time));
+
+        time = System.currentTimeMillis();
+        Long sum2 = 0L;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            sum2 += i;
+        }
+        System.out.println("Time(wrapper) : "
+                + (System.currentTimeMillis() - time));
+    }
+}
