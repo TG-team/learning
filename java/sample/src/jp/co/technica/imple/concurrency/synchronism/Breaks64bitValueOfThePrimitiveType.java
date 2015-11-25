fa440f79b905cb1aa669963feafc41abe04e07b7
taisuke-uchida <uchida.taisuke@gmail.com>
2015-11-25 23:37:10 +0900

    32ビットJavaで代入エラーが発生するサンプルコードの修正

--- java/sample/src/jp/co/technica/imple/concurrency/synchronism/Breaks64bitValueOfThePrimitiveType.java
+++ java/sample/src/jp/co/technica/imple/concurrency/synchronism/Breaks64bitValueOfThePrimitiveType.java
@@ -5,49 +5,41 @@ package jp.co.technica.imple.concurrency.synchronism;
  * 32bit版Javaならこれで現象がおきる？
  * */
 public class Breaks64bitValueOfThePrimitiveType {
-	static class Content{
-		long brokenValue = 0;
+	private static long longValue = 0;
 
-		public long setValue(int val){
-			brokenValue = val;
-			return brokenValue;
-		}
-	}
+	public static void main(String[] args) throws Exception {
+		final int LOOP = 1000 * 1000 * 1000;
 
-	public static void main(String[] args){
-		Content c = new Content();
-
-		Thread t1 = new Thread(createRunnable(1, c));
-		Thread t2 = new Thread(createRunnable(-1, c));
-
-		t1.start();
-		t2.start();
-
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-		System.out.println(c.brokenValue);
-		System.out.println("終了");
-
-	}
-
-	public static Runnable createRunnable(final int val,final Content c){
-		Runnable r = new Runnable() {
-
-			@Override
+		Thread th1 = new Thread(new Runnable() {
 			public void run() {
-				for(int i=0;i<20000000;i++){
-					long ret = c.setValue(val);
-					if(ret != 1 && ret != -1){
-						System.out.println("ThreadName = " + Thread.currentThread().getName() + " :  i = " + i + "  :  brokenValue = " + ret);
-					}
+				for (int i = 0; i < LOOP; i++) {
+					longValue = 1;
+					check(longValue);
 				}
 			}
-		};
-		return r;
+		});
+
+		Thread th2 = new Thread(new Runnable() {
+			public void run() {
+				for (int i = 0; i < LOOP; i++) {
+					longValue = -1;
+					check(longValue);
+				}
+			}
+		});
+
+		th1.start();
+		th2.start();
+
+		th1.join();
+		th2.join();
+
+		System.out.println("終了");
+	}
+
+	private static void check(long value) {
+		if (value != 1 && value != -1) {
+			throw new RuntimeException(String.valueOf(value));
+		}
 	}
 }
